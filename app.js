78661a6be2297962b5437ea3689c103f01c43050
Simarjit Singh <CTSIMARJIT@GMAIL.COM>
2024-07-08 22:24:51 +0530

    keep practice

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading=React.createElement("h1",{ id:"header"},"hello world by simar!");
+//REACT ELEMENTS 
+
+// const heading=React.createElement("h1",{ id:"header"},"hello world by react!");
+
+const chechking=<h1>yes this is what is </h1>
+const jsxheading=<h1 id="heading" className="head">{chechking}this is a simar</h1>
+
 
 const access=ReactDOM.createRoot(document.getElementById("container"));
+// access.render(jsxheading);
 
-access.render(heading);
+
+//REACT COMPONENTS
+
+const Headercomponent=()=><h1 id="insideComponent">this is a simar component</h1>;
+
+const InsideComponent=()=>{    
+return(
+
+    <div>
+        <h1>this component inside component in react</h1>
+        {jsxheading}
+        {Headercomponent()}
+        <Headercomponent></Headercomponent>
+    </div>
+)
+      
+};
+
+access.render(<InsideComponent/>)
